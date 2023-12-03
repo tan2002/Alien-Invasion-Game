a80951549535a2cc0210a31d7fb42cd2d8da2fb0
tan2002 <tanmaydhanvijay0586@gmail.com>
2023-12-04 03:25:15 +0530

    Shrink code size and create new bullet

--- settings.py
+++ settings.py
@@ -2,14 +2,12 @@ class Settings:
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230,230,250)
+        self.bg_color = (230, 230, 250)
         self.ship_speed = 1.5
 
-        self.bullet_speed1 = 1.0
-        self.bullet_speed2 = 1.0
-        self.bullet_width1 = 3
-        self.bullet_width2 = 3
-        self.bullet_height1 = 15
-        self.bullet_height2 = 15
-        self.bullet_color1 = (60,60,60)
-        self.bullet_color2 = (60,60,60)
+        self.bullet1 = (1.0, (173, 215, 230))
+        self.bullet2 = (1.0, (173, 215, 230))
+
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 8
+        self.fleet_direction = 1
