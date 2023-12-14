bfb8ac176ee1ded9426143ab58a2683f45d77499
tan2002 <tanmaydhanvijay0586@gmail.com>
2023-12-15 01:40:51 +0530

    Centralize the ship and update its speed

--- ship.py
+++ ship.py
@@ -1,6 +1,9 @@
 import pygame
-class Ship:
+from pygame.sprite import Sprite
+
+class Ship(Sprite):
     def __init__(self,ai_game):
+        super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
@@ -24,3 +27,7 @@ class Ship:
     
     def blitme(self):
         self.screen.blit(self.image,self.rect)
+
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
